d7d1e612b2af18c64579b2e845ee05cd2db59fc1
anonguy912@gmail.com <>
2020-07-20 21:07:54 +0530

    Changes to be committed: 	new file:   __pycache__/api.cpython-36.pyc 	new file:   __pycache__/text.cpython-36.pyc 	new file:   text.py 	new file:   todo.txt 	new file:   wdc_bot.py
     Changes not staged for commit:
    	modified:   ../bot.py
    	deleted:    ../bot/__pycache__/text.cpython-36.pyc
    	deleted:    ../bot/api.py
    	deleted:    ../bot/text.py
    	deleted:    ../bot/todo.txt
    	deleted:    ../bot/wdc_bot.py

---
+++ bots/text.py
@@ -0,0 +1,37 @@
+abouth="""hi"""
+
+rule="""1. The group is English-only.
+2. Be civil, respect each other. Racism and other forms of discrimination are forbidden.
+3. No advertisement of any kind without proper permission from the admins, unless you're sharing an open-source project. Mind rule 5 when sharing.
+4. No referral links.
+5. No spam, no flood.
+6. No NSFW content.
+7. No contacts, locations sharing allowed in the group.
+
+Before asking something, google first. If you want Web Development tutorials/courses/books see @CodeWithOlivia.
+
+Nobody will do your homework for you.
+
+If you did everything you could and still have to ask, show your advance. If you're getting an error, post full error message and traceback.
+
+Don't beg too much for help. If some people are willing to help, they will."""
+stupi=['is','is not']
+kic='Oh hey you were kiked if you think is a foult contact @Olivia2016 '
+bu='Oh hey you were banned/unbaned if you think is a foult contact @Olivia2016 '
+
+help_=(
+    'Oh hy i am WebDevGuard and i guard the @WebDevChat group\n'
+    '/hi -says hello\n'
+    '/help -this text\n'
+    '/bye -send bot to sleep\n'
+    '/rules - WebDevChat rules\n'
+    '/todo - what parts are working\n'
+    '/stupid <user_id> - says if a user is stupid\n'
+    '/kick <user_id> - bans a user\n'
+    '/kill <mgg_id> -delets a mesage\n'
+    '/whois <user_id> - tells info abouth a user\n'
+    '/members -member count\n'
+    '/unpin -unpin a message\n'
+    '/pardon <user_id> - unban a user\n'
+    '/mute <user_id> - mute a user for 24 hours\n'
+    )
---
+++ bots/wdc_bot.py
@@ -0,0 +1,108 @@
+from pyrogram import Client,Filters,ChatPermissions
+from text import *
+from random import* 
+import time
+
+##init##
+try:
+    import api
+    API_TOKEN=api.TOKEN
+
+except ImportError:
+    API_TOKEN=os.environ['API_TOKEN']
+
+print(API_TOKEN)
+
+app = Client(
+    "bot",
+    bot_token=API_TOKEN
+)
+
+
+##COMMANDS##
+def send(msg_get,msg_send):
+    app.send_message(
+        chat_id=msg_get.chat.id,
+        text=msg_send,
+        reply_to_message_id=msg_get.message_id
+        )
+
+
+@app.on_message(Filters.command("hi"))
+def hi(client,message):
+    send(message,'hello')
+
+@app.on_message(Filters.command("start"))
+def start(client,message):
+    send(message,abouth)
+
+@app.on_message(Filters.command("help"))
+def help(client,message):
+    send(message,help_)
+
+@app.on_message(Filters.command("bye"))
+def bye(client,message):
+    send(message,"bye i am going to sleep")
+
+@app.on_message(Filters.command("rules"))
+def rules(client,message):
+    send(message,rule)
+
+@app.on_message(Filters.command(["test","num"]))
+def test(client,message):
+    send(message,message.command)
+
+@app.on_message(Filters.command('todo'))
+def todo(client,message):
+    f=open('todo.txt',"r")
+    a=f.read()
+    f.close()
+    send(message,a)
+
+@app.on_message(Filters.command(['stupid','id']))
+def stupid(client,message):
+    i=choice(stupi)
+    if message.command[1] is '@Arydev' or 'Arydev':
+        i='is not'
+    send(message,f"{message.command[1]} {i} stupid")
+
+@app.on_message(Filters.command(['kick','id']))
+def kick(client,message):
+    app.kick_chat_member(
+        chat_id=message.chat.id,
+        user_id=message.command[1]
+        )
+    send(message,f"{message.command[1]} is kiked")
+    app.send_message(message.command[1],kic)
+
+@app.on_message(Filters.command('kill'))
+def delete(client,message):
+    message.reply_to_message.delete()
+
+@app.on_message(Filters.command(["whois",'id']))
+def whois(client,message):
+    send(message,app.get_users(message.command[1]))
+
+@app.on_message(Filters.command('members'))
+def memb(client,message):
+    send(message,app.get_chat_members_count(message.chat.id))
+
+@app.on_message(Filters.command('unpin'))
+def mes_count(client,message):
+    app.unpin_chat_message(message.chat.id)
+
+@app.on_message(Filters.command(['pardon','id']))
+def pardon(client,message):
+    app.unban_chat_member(message.chat.id,message.command[1])
+    send(message,f"{message.command[1]} is unbaned")
+    app.send_message(message.command[1],bu)
+
+@app.on_message(Filters.command(['mute24','id']))
+def mute24(client,message):
+    app.restrict_chat_member(message.chat.id, message.command[1], ChatPermissions(), int(time.time() + 86400))
+    send(message,f"{message.command[1]} is muted for 24 hours")
+    app.send_message(message.command[1],bu)
+
+
+app.run()
+
